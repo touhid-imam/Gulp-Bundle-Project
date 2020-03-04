ef89f259a0e297bb5adba00f79c215d927447525
RootCoder <touhid.imam1337@gmail.com>
2020-03-05 00:15:25 +0600

    4.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 const gulp = require('gulp');
 const run = require('gulp-run');
+const fs = require('fs');
 
 
 function pushTags(success){
-	const tag = 'Version-1.0';
-	run(`git tag ${tag}`).exec();
-	run(`git add .`).exec();
-	run(`git commit -m ${tag}`).exec();
-	run(`git push origin master`).exec();
+	const p = fs.readFileSync('index.html', 'utf-8');
+	const versionFind = p.match(/\d\.\d\.\d/g);
+	run(`git tag ${versionFind}`).exec()
+    run( `git push origin ${versionFind}` ).exec();
+    run(`git add .`).exec();
+    run(`git commit -m ${versionFind}`).exec();
+    run(`git push origin master`).exec();
 	success();
 }
 
